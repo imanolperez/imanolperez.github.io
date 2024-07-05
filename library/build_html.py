a612731761b2e5a16a58d191e27732a47f533236
imanol.perez <iperezarribas@LAPTOP-NGHOF7Q8.localdomain>
2024-07-05 21:55:15 +0100

    update library

--- library/build_html.py
+++ library/build_html.py
@@ -14,6 +14,7 @@ def get_cover(book):
   return cover
 
 def add_groups(html, library):
+  print(library["group"].unique())
   groups = sorted(library["group"].unique())
   
   sep = "&nbsp;&nbsp;<small>●</small>&nbsp;&nbsp;"
